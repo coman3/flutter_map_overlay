91ddbfe2d9b01f2966394384e310208acf6817b6
Lachlan van der Velden <me@lachlanvelden.com>
2019-05-22 14:25:19 +1000

    add example project

---
+++ example/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map_overlay/google/google.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.4223705, -122.0843794),
+    zoom: 10.4746,
+  );
+  static final CameraPosition _kNearGooglePlex = CameraPosition(
+    target: LatLng(37.4084642, -122.0717857),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMapOverlay(
+        mapOptions: GoogleMapOptions(initialCameraPosition: _kGooglePlex),
+        overlays: <MapOverlayWidgetContainer>[
+          MapOverlayWidgetContainer(
+            offset: Offset(0, 0),
+            position: _kGooglePlex.target,
+            child: Container(
+              child: Text("Google Plex"),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    style: BorderStyle.solid,
+                    width: 1,
+                    color: Colors.black,
+                  )),
+            ),
+          ),
+          MapOverlayWidgetContainer(
+            offset: Offset(0, 0),
+            position: _kNearGooglePlex.target,
+            child: Text("Near Google Plex"),
+          )
+        ],
+      ),
+    );
+  }
+}
